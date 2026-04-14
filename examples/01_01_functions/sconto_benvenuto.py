8bbcde83e2aa3831bd008b882343afb7681ff6ef
studpozzolid3a25 <stud.pozzolid@gdromagnosi.edu.it>
2026-04-14 09:51:09 +0000

    scont_benvenuto

---
+++ examples/01_01_functions/sconto_benvenuto.py
@@ -0,0 +1,7 @@
+
+def sconto_benvenuto(prezzo_pantaloni, prezzo_maglietta, prezzo_scarpe,prezzo_finale):
+  prezzo_pantaloni=50
+  prezzo_maglietta=20
+  prezzo_scarpe=80
+  print("cosa uoi comprare?")
+  print("prezzo_finale=prezzo_pantaloni-5")
